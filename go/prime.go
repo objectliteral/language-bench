c055b81157759f89e0e896de1b375c09a0fe8009
Peter Steinberg <objectliteral@outlook.com>
2018-01-21 05:33:42 +0100

    Added Go implementation

---
+++ go/prime.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	stopValue := 10000000
+	cnt := countPrimes(stopValue)
+	fmt.Printf("There are %d prime numbers between 0 and %d\n", cnt, stopValue)
+}
+
+func countPrimes(until int) int {
+
+	count := 1
+
+	for i := 3; i <= until; i += 2 {
+
+		isPrime := true
+		t := 3
+
+		for t <= int(math.Sqrt(float64(i))) {
+			if i%t == 0 {
+				isPrime = false
+				break
+			}
+			t += 2
+		}
+
+		if isPrime == true {
+			count++
+		}
+	}
+	return count
+}
---
+++ go/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+go build prime.go && time ./prime
